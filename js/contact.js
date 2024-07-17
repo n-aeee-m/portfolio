b7cb46dcc07482a080d94c8a583b0fdef23d3e72
naeeemkk <naeemkk10000@gmail.com>
2024-07-17 12:36:42 +0530

    17 12:36

--- js/contact.js
+++ js/contact.js
@@ -1,21 +1,38 @@
 function sendMail(event) {
-    event.preventDefault();
+  event.preventDefault(); // Prevent form submission
 
-    var params = {
-      name: document.getElementById("InputName").value,
-      email: document.getElementById("InputEmail").value,
-      message: document.getElementById("InputMessage").value,
-    };
-    const serviceID = "service_mxv9fwa";
-    const templateID = "template_8lm2uvk";
+  var name = document.getElementById('InputName').value.trim();
+  var email = document.getElementById('InputEmail').value.trim();
+  var message = document.getElementById('InputMessage').value.trim();
 
-    emailjs.send(serviceID, templateID, params)
-      .then(res => {
-          document.getElementById("InputName").value = "";
-          document.getElementById("InputEmail").value = "";
-          document.getElementById("InputMessage").value = "";
-          console.log(res);
-          alert("Your message sent successfully!!");
-      })
-      .catch(err => console.log(err));
+  // Check if any field is empty
+  if (name === '' || email === '' || message === '') {
+      // Display an alert message
+      alert('Please fill out all fields.');
+      return; // Exit function if any field is empty
+  }
+
+  const serviceID = 'service_mxv9fwa'; // Replace with your actual service ID
+  const templateID = 'template_8lm2uvk'; // Replace with your actual template ID
+  var params = {
+      name: name,
+      email: email,
+      message: message
+  };
+
+  // Sending email using emailjs library
+  emailjs.send(serviceID, templateID, params)
+    .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+        // Clear form fields after successful submission
+        document.getElementById('InputName').value = '';
+        document.getElementById('InputEmail').value = '';
+        document.getElementById('InputMessage').value = '';
+        // Display success alert
+        alert('Your message sent successfully!');
+    }, function(error) {
+        console.log('FAILED...', error);
+        // Handle errors here
+        alert('Failed to send message. Please try again later.');
+    });
 }
